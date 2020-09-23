d23e12d98aaad863142322fd8326eb20740d6149
bagasafrz7 <bagasafrz16@gmail.com>
2020-09-23 12:07:23 +0700

    feat: Add Login Page

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <main>
+      <b-container>
+        <b-row>
+          <b-col cols="12" md="12" sm="12">
+            <div class="main-login">
+              <h3>Login</h3>
+              <h6>Hi, Wellcome back!</h6>
+              <b-form>
+                <b-form-group id="input-group-1" label="Email" label-for="input-1">
+                  <b-form-input id="input-1" type="email" required placeholder="Enter Your Email"></b-form-input>
+                </b-form-group>
+
+                <b-form-group id="input-group-2" label="Password" label-for="input-2">
+                  <b-form-input
+                    id="input-2"
+                    type="password"
+                    required
+                    placeholder="Enter Your Password"
+                  ></b-form-input>
+                </b-form-group>
+
+                <router-link to="/forgot-password">
+                  <p class="text-right">Forgot password?</p>
+                </router-link>
+
+                <b-button type="submit" class="btn-login" variant="primary">Login</b-button>
+                <b-row class="my-4">
+                  <b-col cols="4" md="4" sm="4">
+                    <hr />
+                  </b-col>
+                  <b-col cols="4" md="4" sm="4" class="text-center">
+                    <span>Login with</span>
+                  </b-col>
+                  <b-col cols="4" md="4" sm="4">
+                    <hr />
+                  </b-col>
+                </b-row>
+                <b-button type="button" class="btn-google">Google</b-button>
+                <p class="text-center my-2">
+                  Don't have an account?
+                  <router-link to="/register">Sign Up</router-link>
+                </p>
+              </b-form>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+main {
+  background: #e5e5e5;
+}
+.main-login {
+  width: 50%;
+  background: #fff;
+  margin: 50px auto;
+  padding: 25px 50px;
+  border-radius: 30px;
+}
+.main-login h3 {
+  color: #7e98df;
+  text-align: center;
+}
+.main-login h6 {
+  color: #232323;
+}
+label {
+  color: #848484;
+  margin-top: 25px;
+}
+.main-login .form-control {
+  border: none;
+  border-bottom: 1px solid black;
+  border-radius: 0;
+  outline: none;
+}
+.main-login .btn-login {
+  width: 100%;
+  padding: 10px 25px;
+  border-radius: 25px;
+  background: #7e98df;
+  border: 1px solid #7e98df;
+}
+hr {
+  background: #848484;
+}
+.main-login .btn-google {
+  width: 100%;
+  border-radius: 25px;
+  padding: 10px 25px;
+  border: 1px solid #7e98df;
+  background: #fff;
+  color: #7e98df;
+}
+</style>
